6cb8ca79d8b543e70a80c2e8b3eb044f4a42289f
Ravi Rathore <www.ravi3598@gmail.com>
2024-01-13 21:40:59 +0530

    change

--- src/App.js
+++ src/App.js
@@ -12,8 +12,7 @@ function App() {
       <div className="App">
           <BrowserRouter>
         <Routes>
-        <Route path='/' element={<SignIn />}></Route>
-          <Route path='/Home' element={<Home />}></Route>
+          <Route path='/' element={<Home />}></Route>
           <Route path='/SignUp' element={<SignUp />}></Route>
           <Route path='/SignIn' element={<SignIn />}></Route>
         </Routes>
